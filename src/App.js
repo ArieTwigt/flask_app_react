455246388771eb437551725b18eba809b85bc31b
arietwigt <twigt.arie@gmail.com>
2021-07-13 21:13:55 +0200

    added walking skeleton for flask app with react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentCar, setCurrentCar] = useState("No Car");
+
+  useEffect(() => {
+    fetch('/random_cars').then(res => res.json()).then(data => {
+      setCurrentCar(data[0]);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <h1>Random auto selector </h1>
+      <hr></hr>
+        <h3>Merk: {currentCar.merk}.</h3>
+        <p>Model: {currentCar.handelsbenaming}.</p>
+        <p>Kenteken: {currentCar.kenteken}.</p>
       </header>
     </div>
   );
